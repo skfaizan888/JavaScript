b1596335898c476d4ad890b83fe8de718035908b
furqana@275@gmail.com <github@275>
2021-10-14 18:34:57 +0530

    Comment

--- veryImp/construct.js
+++ veryImp/construct.js
@@ -1,3 +1,6 @@
+//method -> obj
+//function -> global (window,global)
+
 function Circle(radiuss) {
     this.radius = radiuss;    //radius:4
     this.pi = 3.14;    // pi: 3.14
@@ -5,10 +8,10 @@ function Circle(radiuss) {
         return this.pi * this.radius * this.radius;
     }
 }
-const result = new Circle(4);  //result ={Shaikh_Faizan_Ahmed...888}
+const result = new Circle(4);
 console.log(result.area());
 
-
+//Second Example
 const movies = {
     title: "abc",
     tags: [1, 4, 7, 5],
@@ -19,4 +22,16 @@ const movies = {
 
     }
 }
-movies.showMovie()
+movies.showMovie();
+
+
+const Video = {
+    title: "AB",
+    age: [5, 6, 7, 8],
+    showVideo() {
+        this.age.forEach(function (vid) {
+            console.log(this.title, vid)
+        }, this)
+    }
+}
+Video.showVideo();
